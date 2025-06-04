068415f652d8342168279187eeab29e6814bb830
Jingmin Liang <leungjimmy82218@hotmail.com>
2025-06-04 14:07:24 +0800

    Initial commit

---
+++ flightticketing-service-api/src/main/java/org/flightticketing/flightsapi.java
@@ -0,0 +1,9 @@
+package org.flightticketing;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class flightsapi {
+}
